1a257e0ec3f1747f36bfb4143ac5a77acd06b5f7
Scott Greenwald <scottgwald@media.mit.edu>
2015-07-27 06:01:54 +0200

    pipeline works to generate pdf frames

--- mel-from-pymel.py
+++ mel-from-pymel.py
@@ -1,97 +1,4 @@
-from pymel.all import *
-import sets
-import os
-from os.path import expanduser
-
-# config variables
-
-repo_rel_path = os.path.join("mygit","pymel-svg-hack") # path from user home folder
-point_order_file = os.path.join("maya-output", "mouth-point-order")
-
-home = expanduser("~")
-repo_full_path = os.path.join( home, repo_rel_path )
-point_order_file_full_path = os.path.join( repo_full_path, point_order_file )
-
-mesh_vertices = ls(selection=True)
-print len(mesh_vertices)
-print mesh_vertices
-
-vertex_set = sets.Set();
-print "Initial size of vertex_set " + str(vertex_set.__len__())
-vertex_array = [];
-
-for vert_range in mesh_vertices:
-	print vert_range
-	for vert in vert_range:
-		print "Adding vertex " + str(vert) + " to vertex set."
-		starting_size = vertex_set.__len__()
-		print "starting_size is " + str(starting_size)
-		vertex_set.add(vert)
-		vertex_array.append(vert)
-		if starting_size == vertex_set.__len__():
-			print "Looks like vertex " + str(vert) + "was already in the set!!"
-		# print vert
-		# print "connected vertices: " + vert.connectedVertices()
-
-print "size of vertex set " + str(vertex_set.__len__())
-print vertex_set
-print "length of vertex array is " + str(len(vertex_array))
-print vertex_array
-
-ordered_output_array = []
-
-#first_vertex = vertex_set.pop()
-#ordered_output_array.append(first_vertex)
-
-current_vertex = vertex_set.pop()
-ordered_output_array.append(current_vertex)
-
-while vertex_set.__len__() > 0:
-	neighbors_range = current_vertex.connectedVertices()
-	neighbors = []
-	for neighbor in neighbors_range:
-		neighbors.append(neighbor)
-	neighbor_vertex = neighbors.pop()
-	while not vertex_set.__contains__(neighbor_vertex):
-		neighbor_vertex = neighbors.pop()
-	ordered_output_array.append(neighbor_vertex)
-	print "Found neighbor " + neighbor_vertex + " of current vertex " + current_vertex + "."
-	current_vertex = neighbor_vertex;
-	vertex_set.remove(current_vertex)
-
-print ordered_output_array
-string_for_mel = "{\"" + "\",\"".join(map(str, ordered_output_array)) + "\"}"
-print "string for mel is:\n" + string_for_mel
-print os.getcwd()
-print point_order_file_full_path
-
-string_for_file = "\n".join(map(str, ordered_output_array))+"\n"
-
-# f = open( point_order_file_full_path, 'w' )
-filename = point_order_file_full_path
-dir = os.path.dirname(filename)
-if not os.path.exists(dir):
-    os.makedirs(dir)
-with open(filename, 'w') as f:
-	print "writing point order to " + filename
-	f.write(string_for_file)
-
-# Algorithm:
-# select a vertex (A, the first one), add it to the ordered result list (result_list)
-# seek a neighbor (B) that is a member of the vertex set. Add it to the ordered result list, and remove it from the vertex set
-# seek a neihbor of B that is a member of the vertex set, and not a member of the ordered result list
-# iterate until vertex set is empty
-
-# mel.eval("""
-
-# MEL SCRIPT HERE ...
-
-# %s
-
-# """
-# % string_for_mel
-
-# )
+from pymel.core import *
 
 mel_screenSpace="""
 // Get a matrix
@@ -116,9 +23,8 @@ global proc int screenSpace(string $sCam)
 {
   string $folderPath = "/Users/scottgwald/mygit/pymel-svg-hack/maya-output/";  
   // get the currently selected point
-  string $dumpList[] = %s;
-  // string $dumpList[] = {"Head_MeshShape.vtx[5486]", "Head_MeshShape.vtx[1865]", "Head_MeshShape.vtx[1864]"};
-  // string $dumpList[] = `ls -sl`;
+  string $dumpList[] = [%s]
+  string $dumpList[] = `ls -sl`;
   int $argc = size($dumpList);
 
   // string orderedListFromPython = [Head_MeshShape.vtx[1723],Head_MeshShape.vtx...]
@@ -236,7 +142,32 @@ button -edit -command ("string $cSelection[1] = `textScrollList -query -selectIt
 showWindow $myWin;
 """
 
-mel.eval(mel_screenSpace % string_for_mel)
-mel.eval(mel_ui)
+mel_ui_fn="""
+global proc myMouthScript() {
+	// Make UI
+	string $myWin = `window -widthHeight 600 300 -title "Maya2ToonBoom"`;
+	columnLayout -adjustableColumn true;
+	text -label "First select the object, then select one camera below.\\nProceed when ready";
+	separator -height 10 -style "double";
+	// List cameras
+	string $myCams[] = `listCameras -p`;
+	int $numCam = size($myCams);
+	// Build list
+	$myList = `textScrollList -height 200 -numberOfRows 1 -allowMultiSelection false`;
+	int $i;
+	for ($i = 0; $i < $numCam; $i++) {
+	  textScrollList -edit -append $myCams[$i] $myList;
+	}
+	textScrollList -edit -selectIndexedItem 1 $myList;
+	separator -height 10 -style "single";
+	// OK button
+	$myButton = `button -label "OK" -height 50 -align "center"`;
+	button -edit -command ("string $cSelection[1] = `textScrollList -query -selectItem $myList`; screenSpace $cSelection[0];") $myButton;
+	showWindow $myWin;
+}
+"""	
 
+mel.eval(mel_screenSpace)
+mel.eval(mel_ui)
+# mel.myMouthScript()
 
